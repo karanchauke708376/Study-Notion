88ae3b2584e6faa05df997bfa794c18992d6e47d
Karan Chauke <69950796+karanchauke708376@users.noreply.github.com>
2023-09-27 15:40:55 +0530

    Add Modify Name MONGODB

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGOdb_URL 
+    mongoose.connect(process.env.MONGODB_URL 
         
     )
     .then(() => console.log("DB Connected Successfully"))
